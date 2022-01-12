f8963ec5b5f6dd9d4ab7249db7e0c9d96b9a3f75
Dina Abdelbary <63638151+Dinaabdelbary@users.noreply.github.com>
2022-01-12 21:17:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("connected", data);
+
+let container = document.getElementById('container');
+
+
+for (let i = 0; i < data.length; i++) {
+let myDiv = document.createElement('div')
+
+myDiv.innerHTML = `
+
+<div>
+<img src ="${data[i].image}">
+<p>question: ${i + 1}</p>
+<p>${data[i].question}</p>
+<input class = "possibleAnswer" type="text" />
+<button onclick="checkAnswer(event,'${data[i].answer}')"> Answer the question <button/>
+</div>
+
+`
+container.appendChild(myDiv)
+}
+
+// const checkAnswer = () => {
+//     console.log('clicked')
+// }
+
+
+function checkAnswer(event,answer) {
+    console.log("answer",answer);
+   let possibleAnswer = event.target.parentElement.querySelector('.possibleAnswer').value
+   if (possibleAnswer.toLowerCase() === answer.toLowerCase()) {
+    event.target.parentElement.querySelector(".result").innerHTML="nice"
+   }else{
+    event.target.parentElement.querySelector(".result").innerHTML="wrong"
+
+   }
+}
+
+
